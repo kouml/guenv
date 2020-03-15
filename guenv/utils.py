71a1f791d6b52530181669c03fa54436fe227faa
kouml <key.mtrgtring@gmail.com>
2020-03-15 20:01:10 +0900

    update for initialize procedure

--- guenv/utils.py
+++ guenv/utils.py
@@ -1,3 +1,4 @@
+import os
 import json
 import subprocess
 
@@ -16,10 +17,20 @@ def save_activate(activated_config):
 
 
 def load_config():
-    with open(ACTIVATE_PATH, 'r') as f:
-        activated_config = f.read().strip()
-    with open(CONFIG_PATH, 'r') as f:
-        config_list = json.load(f)
+    if os.path.isfile(ACTIVATE_PATH):
+        with open(ACTIVATE_PATH, 'r') as f:
+            activated_config = f.read().strip()
+    else:
+        with open(ACTIVATE_PATH, 'a') as f:
+            activated_config = ""
+
+    if os.path.isfile(CONFIG_PATH):
+        with open(CONFIG_PATH, 'r') as f:
+            config_list = json.load(f)
+    else:
+        with open(CONFIG_PATH, 'a') as f:
+            config_list = {}
+
     return activated_config, config_list
 
 
