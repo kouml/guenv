f8ccc315dbb6c1b996bbe2e44606168b13f78769
kouml <key.mtrgtring@gmail.com>
2020-03-15 16:53:35 +0900

    add utils

---
+++ guenv/utils.py
@@ -0,0 +1,45 @@
+import json
+
+CONFIG_PATH = 'config_all.json'
+ACTIVATE_PATH = 'activate'
+
+
+# io utils
+def save_config(config_list):
+    with open(CONFIG_PATH, 'w') as f:
+        json.dump(config_list, f)
+
+
+def save_activate(activated_config):
+    with open(ACTIVATE_PATH, 'w') as f:
+        f.write(activated_config)
+
+
+def load_config():
+    # load activate
+    with open(ACTIVATE_PATH, 'r') as f:
+        activated_config = f.read().strip()
+    # load config
+    with open(CONFIG_PATH, 'r') as f:
+        config_list = json.load(f)
+    return activated_config, config_list
+
+
+# git env
+def git_config_set():
+    activated_config, config_list = load_config()
+    user_name = config_list[activated_config]['user_name']
+    email = config_list[activated_config]['email']
+
+
+def my_decorator(func):
+    def wrapper():
+        func()
+        git_config_set()
+    return wrapper
+
+
+@my_decorator
+def say_whee():
+    print("Whee!")
+
